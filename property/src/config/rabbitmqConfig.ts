1036ac5432ba15a7d35591fd0e9b6ce1225671b4
zayed17 <mozayed7736gmail.com>
2024-10-27 14:19:17 +0530

    few commented code removed

--- property/src/config/rabbitmqConfig.ts
+++ property/src/config/rabbitmqConfig.ts
@@ -1,15 +1,6 @@
-// export const rabbitmqConfig = {
-//     url: process.env.RABBITMQ_URL || "amqp://rabbitmq:5672",
-//     exchange: 'user_updates', 
-//     queues: {
-//         updateUser: 'user_updates_queue', 
-//     }
-// };
-
 export const rabbitmqConfig = {
     url: "amqp://rabbitmq:5672",
 
-    // Exchange and queue configuration for user updates
     exchanges: {
         userUpdates: 'user_updates',
         bookingUpdates: 'booking_exchange' 
@@ -17,11 +8,11 @@ export const rabbitmqConfig = {
 
     queues: {
         updateUser: 'user_updates_queue',
-        bookings: 'bookings_queue' // New queue for booking updates
+        bookings: 'bookings_queue' 
     },
 
     routingKeys: {
         userUpdateKey: 'update',
-        bookingKey: 'booking_key' // New routing key for booking updates
+        bookingKey: 'booking_key' 
     }
 };
