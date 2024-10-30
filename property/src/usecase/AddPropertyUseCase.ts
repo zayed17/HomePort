502accb772cf74436e51d06450f029de4e4dbee9
zayed17 <mozayed7736gmail.com>
2024-10-30 11:09:39 +0530

    property add bug fixed

--- property/src/usecase/AddPropertyUseCase.ts
+++ property/src/usecase/AddPropertyUseCase.ts
@@ -3,7 +3,7 @@ import { Property } from '../entities/propertyEntity';
 
 export class AddPropertyUseCase {
   constructor(
-    // private s3Repository: S3Interface,
+    private s3Repository: S3Interface,
     private propertyRepository: PropertyInterface,
     private rabbitMQPublisher: RabbitMQPublisherInterface
   ) { }
@@ -11,13 +11,13 @@ export class AddPropertyUseCase {
   async addProperty(files: Express.Multer.File[], formData: any,id:string): Promise<Property> {
     console.log(files, "checking from usecase");
 
-    // const uploadPromises = files.map((file) => this.s3Repository.uploadImage(file));
-    // const uploadedFiles = await Promise.all(uploadPromises);
-    // const imageUrls = uploadedFiles.map((file) => file);
+    const uploadPromises = files.map((file) => this.s3Repository.uploadImage(file));
+    const uploadedFiles = await Promise.all(uploadPromises);
+    const imageUrls = uploadedFiles.map((file) => file);
     formData.createdBy = id
     const propertyData: Property = {
       ...formData,
-      // mediaFiles: imageUrls,
+      mediaFiles: imageUrls,
     };
     await this.rabbitMQPublisher.publish('user_updates', 'update', { id,count:1,change:'postedProperty' });
     return this.propertyRepository.addProperty(propertyData);
