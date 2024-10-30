2514e57762bdb8d5fa49cb2112c5938fbd5de1c2
zayed17 <mozayed7736gmail.com>
2024-10-30 11:48:19 +0530

    code clean

--- Client/src/hooks/useUserDetails.ts
+++ Client/src/hooks/useUserDetails.ts
@@ -1,18 +1,3 @@
-// import { useEffect } from 'react';
-// import { useGetUserQuery } from '../store/user/userApi';
-
-// export const useUserDetails = () => {
-//   const { data: { userDetails } = {}, isLoading, isError, error } = useGetUserQuery({});
-
-//   useEffect(() => {
-//     if (isError) {
-//       console.error('Error fetching user details:', error);
-//     }
-//   }, [isError, error]);
-
-//   return { userDetails, isLoading, isError };
-// };
-
 import { useEffect, useState } from 'react';
 import { useGetUserQuery } from '../store/user/userApi';
 
@@ -23,9 +8,8 @@ export const useUserDetails = () => {
   useEffect(() => {
     if (isError && !hasLoggedError) {
       console.error('Error fetching user details:', error);
-      setHasLoggedError(true); // Set the flag to true to avoid re-logging the same error
+      setHasLoggedError(true); 
     }
-    // Optionally, you might want to reset the flag if `isError` is false again
   }, [isError, error, hasLoggedError]);
 
   return { userDetails, isLoading, isError };
--- Client/src/routes/Protected.tsx
+++ Client/src/routes/Protected.tsx
@@ -1,37 +1,3 @@
-// import React, { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { getCookie } from "../helpers/getCookie";
-
-// interface ProtectedRouteProps {
-//   element: React.ComponentType;
-//   cookieName: string;
-//   redirectTo: string;
-// }
-
-// const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
-//   element: Component,
-//   cookieName,
-//   redirectTo,
-// }) => {
-//   const navigate = useNavigate();
-//   const token = getCookie(cookieName);
-// console.log(token)
-//   useEffect(() => {
-//     if (!token) {
-//       navigate(redirectTo);
-//     }
-//   }, [token, navigate, redirectTo]);
-
-//   if (!token) {
-//     return null;
-//   }
-
-//   return <Component />;
-// };
-// export default ProtectedRoute;
-
-
-
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCookie } from "../helpers/getCookie";
